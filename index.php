987a5c432dfc7d54f24df5537a9e6a665ee92763
LorindeLepper <liedellaar@gmail.com>
2022-02-22 13:26:05 +0100

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<!-- this is an example initial project page -->
+
+
+
+<h1>New project networking </h1>
+<br>
+
+<?php
+	echo "<your name here>" . "<br>"; 
+
+	echo "Today is " . date("Y-m-d") . "<br>";
+	
+	echo "running on: ";
+	echo php_uname() . "<br>";
+?>
+
+
+</html>
